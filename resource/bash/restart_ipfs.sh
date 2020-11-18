ccc34f6228a98a72116d01b6508664b3760d18ed
Geolffrey Mena <gmjun2000@gmail.com>
2020-11-18 10:16:23 -0600

    Add fix memory error mongo migrate

--- resource/bash/restart_ipfs.sh
+++ resource/bash/restart_ipfs.sh
@@ -3,7 +3,14 @@ forever stopall
 rm hash
 rm clients
 rm -rf orbitdb/
+
+# Clean old pinned data
 ipfs pin ls --type recursive | cut -d' ' -f1 | xargs -n1 ipfs pin rm
 ipfs repo gc
 
+# Fix sort memory error in mongo
+docker-compose exec watchit_mongo mongo --eval 'db.adminCommand({setParameter: 1, internalQueryExecMaxBlockingSortBytes:1048576000})'
+docker-compose exec watchit_mongo mongo --eval 'db.adminCommand({setParameter: 1, internalQueryMaxBlockingSortMemoryUsageBytes:1048576000})'
+
+# Run migration and expose orbit forever
 forever start resource/orbit/migrate.js false
