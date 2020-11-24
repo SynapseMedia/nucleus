fd0a66acdccff562ef2b031f150fb4a650fffe23
Geolffrey Mena <gmjun2000@gmail.com>
2020-11-24 14:53:11 -0600

    Fix sort memory error

--- resource/bash/run.sh
+++ resource/bash/run.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
-forever stopall
 rm hash
 rm client
 rm -rf orbitdb/
-
-# Fix sort memory error in mongo
-docker-compose exec watchit_mongo mongo --eval 'db.adminCommand({setParameter: 1, internalQueryExecMaxBlockingSortBytes:1048576000})' > /dev/null
-docker-compose exec watchit_mongo mongo --eval 'db.adminCommand({setParameter: 1, internalQueryMaxBlockingSortMemoryUsageBytes:1048576000})' > /dev/null
-
-# Run migration and expose orbit forever
+forever stopall
 forever start resource/orbit/migrate.js $1 $2
