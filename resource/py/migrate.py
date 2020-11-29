34a55026a0db784661cd289ddcc528d0a5ec82cb
Geolffrey Mena <gmjun2000@gmail.com>
2020-11-29 15:10:53 -0600

    Subs and files ingestion ready

--- resource/py/migrate.py
+++ resource/py/migrate.py
@@ -18,42 +18,43 @@ if __name__ == '__main__':
     START_PAGE = int(os.environ.get('START_PAGE', 0))
     STEP_PAGE = int(os.environ.get('STEP_PAGE', 50))
     REFRESH_MOVIES = os.environ.get('REFRESH_MOVIES', 'False') == 'True'
+    REFRESH_IPFS = os.environ.get('REFRESH_IPFS', 'False') == 'True'
     REFRESH_SUBS = os.environ.get('REFRESH_SUBS', 'False') == 'True'
 
     # CLI
     _root_api = 'https://yts.mx'
+    _client = MongoClient(f"mongodb://{MONGO_HOST}:{MONGO_PORT}/")
     # Initialize yts.ag
     yts = YTS(
         host='%s/api/v2/list_movies.json' % _root_api,
         page=START_PAGE, limit=STEP_PAGE
     )
 
-
-    def db_factory(db_name):
-        _client = MongoClient(f"mongodb://{MONGO_HOST}:{MONGO_PORT}/")
-        return _client[db_name]
-
-    def rewrite_entries(db, data):
-        db.movies.delete_many({})  # Clean all
+    def rewrite_entries(r_db, data):
+        r_db.movies.delete_many({})  # Clean all
         bulk = [InsertOne(i) for k, i in data.items()]
-        db.movies.bulk_write(bulk)
+        r_db.movies.bulk_write(bulk)
 
     # Setting mongo
     print('\nSetting mongodb')
     print("Running %s version in %s directory" % (DB_DATE_VERSION, ROOT_PROJECT))
-    _mongo_db = db_factory('witth')
+    _mongo_db = _client['witth']
+    _ipfs_db = _client['ipfs']
     _migration_result = []
 
-    # If clean
+    # If empty db
     empty_mongo = _mongo_db.movies.find({}).count() == 0
+    empty_ipfs = _ipfs_db.movies.find({}).count() == 0
+
     if REFRESH_MOVIES or empty_mongo:
         print('Rewriting...')
         print(f"\n{Log.BOLD}Starting migrations from yts.mx {DB_DATE_VERSION}{Log.ENDC}")
         # The result of migration
-        _migration_result = yts.migrate(resource_name='yts')
+        migration_result = yts.migrate(resource_name='yts')
         print(f"{Log.OKGREEN}Migration Complete for yts.ag{Log.ENDC}")
         print(f"{Log.OKGREEN}Inserting entries in mongo{Log.ENDC}")
-        rewrite_entries(_mongo_db, _migration_result)
+        rewrite_entries(_mongo_db, migration_result)
+        print(f"{Log.UNDERLINE}Entries yts indexed: {len(migration_result)}{Log.ENDC}")
 
     if REFRESH_SUBS or empty_mongo:
         # Process subs for each movie
@@ -71,19 +72,13 @@ if __name__ == '__main__':
         subs_lists_open = OSubs(migration_result)
         print(f"{Log.OKGREEN}Migration Complete for open subtitles{Log.ENDC}")
         print(f"{Log.OKGREEN}Save in Mongo OpenSub subs{Log.ENDC}")
-        _migration_result = list(_mongo_db.movies.find({}))
         write_subs(_mongo_db, migration_result, subs_lists_open, 'opensubs')
 
-    if REFRESH_MOVIES or empty_mongo:
-        _migration_result = list(_mongo_db.movies.find({}))
-        _migration_result = process_ingestion(_migration_result)
-        _mongo_db_ipfs = db_factory('ipfs')
-        rewrite_entries(_mongo_db_ipfs, _migration_result)
-
-    if len(_migration_result) > 0:
-        print(f"\n{Log.BOLD}Migration Complete:{Log.ENDC}")
-        print(f"{Log.UNDERLINE}Entries yts indexed: {len(_migration_result)}{Log.ENDC}")
-
+    if REFRESH_IPFS or empty_ipfs:
+        print(f"\n{Log.WARNING}Starting ingestion to IPFS{Log.ENDC}")
+        migration_result = list(_mongo_db.movies.find({}))
+        ingestion_result = process_ingestion(migration_result)
+        rewrite_entries(_ipfs_db, ingestion_result)
 
     # Spawn node subprocess
     # call(["node", "%s/resource/orbit/migrate.js" % ROOT_PROJECT, MONGO_HOST], shell=False)
