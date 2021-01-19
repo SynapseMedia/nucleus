d9b539d425bae2b9fe32df5bb45517f8beced46f
Geolffrey Mena <gmjun2000@gmail.com>
2021-01-19 16:01:03 -0600

    Fix bad signature Buffer => Uint8Array orbit

--- src/orbit/local.js
+++ src/orbit/local.js
@@ -20,7 +20,7 @@ const address = args[0] || fs.readFileSync(
         for await (const cid of ipfs.dht.findProvs(address)) {
             console.info('Connecting to:', cid.id)
             const mAddr = cid.addrs.map((m) => `${m.toString()}/p2p/${cid.id}`)
-
+            console.log(cid);
             for (const m of mAddr) {
                 try {
                     await ipfs.swarm.connect(m, {timeout: 1000})
