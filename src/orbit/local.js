590356851057a38880e3137f3b9103451cfd51a4
Geolffrey Mena <gmjun2000@gmail.com>
2021-01-19 15:59:05 -0600

    Fix bad signature Buffer => Uint8Array orbit

--- src/orbit/local.js
+++ src/orbit/local.js
@@ -2,7 +2,6 @@ const IpfsApi = require('ipfs-http-client');
 const OrbitDB = require('orbit-db')
 const fs = require('fs')
 const path = require('path')
-const { consume } = require('streaming-iterables')
 
 args = process.argv.slice(2);
 const address = args[0] || fs.readFileSync(
@@ -17,6 +16,20 @@ const address = args[0] || fs.readFileSync(
         console.log('Loading db..')
         const ipfs = IpfsApi();
         const orbitdb = await OrbitDB.createInstance(ipfs);
+
+        for await (const cid of ipfs.dht.findProvs(address)) {
+            console.info('Connecting to:', cid.id)
+            const mAddr = cid.addrs.map((m) => `${m.toString()}/p2p/${cid.id}`)
+
+            for (const m of mAddr) {
+                try {
+                    await ipfs.swarm.connect(m, {timeout: 1000})
+                    console.log(`Connected to`, m);
+                } catch (e) {
+                    console.log(`Cannot connect to`, m);
+                }
+            }
+        }
 
         console.log('Starting db movies..')
         const dbAddress = `/orbitdb/${address}/wt.movies.db`;
