0041f54010efb29fb9df8e4a38cdb879b02949b3
geolffrey <gmjun2000@gmail.com>
2021-01-29 19:22:50 -0600

    Check every 2m worker.sh

--- src/bash/init_ipfs.sh
+++ src/bash/init_ipfs.sh
@@ -3,7 +3,6 @@
 docker-compose exec watchit_ipfs ipfs config Addresses.Swarm '["/ip4/0.0.0.0/tcp/4001", "/ip4/0.0.0.0/tcp/4002/ws", "/ip6/::/tcp/4001"]' --json
 docker-compose exec watchit_ipfs ipfs config Peering.Peers '[{ "ID": "12D3KooWQw3vx2E4FKpL9GHC9BpFya1MXVUFEVBAQVhMDkreCqwF", "Addrs": ["/ip4/185.215.224.79/tcp/4001"] }, { "ID": "12D3KooWD4Z47R1pnzTxCVQAiTKTHasWU2xTAcffyC38BNKM68yw", "Addrs": ["/ip4/185.215.227.40/tcp/4001"] }, { "ID": "QmbPFTECrXd7o2HS2jWAJ2CyAckv3Z5SFy8gnEHKxxH52g", "Addrs": ["/ip4/144.172.69.157/tcp/4001"] }]' --json
 docker-compose exec watchit_ipfs ipfs config --bool Swarm.EnableRelayHop false
-#docker-compose exec watchit_ipfs ipfs config --bool Swarm.EnableAutoNATService true
 docker-compose exec watchit_ipfs ipfs config --bool Swarm.EnableAutoRelay true
 ##docker-compose exec watchit_ipfs ipfs config --bool Discovery.MDNS.Enabled true
 
