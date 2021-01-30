48b35399b342d47be38f5026014dedb8f12f67ec
geolffrey <gmjun2000@gmail.com>
2021-01-29 18:10:28 -0600

    Check every 2m worker.sh

--- src/bash/init_ipfs.sh
+++ src/bash/init_ipfs.sh
@@ -26,7 +26,7 @@ else
   ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
   ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
   ipfs config Addresses.Swarm '["/ip4/0.0.0.0/tcp/4001", "/ip4/0.0.0.0/tcp/4002/ws", "/ip6/::/tcp/4001"]' --json
-  ipfs config Peering.Peers '[{ "ID": "12D3KooWQw3vx2E4FKpL9GHC9BpFya1MXVUFEVBAQVhMDkreCqwF", "Addrs": ["/ip4/185.215.224.79/tcp/4001"] }, { "ID": "12D3KooWD4Z47R1pnzTxCVQAiTKTHasWU2xTAcffyC38BNKM68yw", "Addrs": ["/ip4/185.215.227.40/tcp/4001"] }, { "ID": "QmbPFTECrXd7o2HS2jWAJ2CyAckv3Z5SFy8gnEHKxxH52g", "Addrs": ["/ip4/144.172.69.157/tcp/4001"] }]' --json
+  ipfs config Peering.Peers '[{"ID":"QmUMxspr3mZZSFiDY3zgLVuUY9kGfS28ZAEUv8nRcan2vS", "Addrs":["/ip4/34.220.216.205/tcp/4001"]}, { "ID": "12D3KooWQw3vx2E4FKpL9GHC9BpFya1MXVUFEVBAQVhMDkreCqwF", "Addrs": ["/ip4/185.215.224.79/tcp/4001"] }, { "ID": "12D3KooWD4Z47R1pnzTxCVQAiTKTHasWU2xTAcffyC38BNKM68yw", "Addrs": ["/ip4/185.215.227.40/tcp/4001"] }, { "ID": "QmbPFTECrXd7o2HS2jWAJ2CyAckv3Z5SFy8gnEHKxxH52g", "Addrs": ["/ip4/144.172.69.157/tcp/4001"] }]' --json
 
   # Set up the swarm key, if provided
   SWARM_KEY_FILE="$repo/swarm.key"
