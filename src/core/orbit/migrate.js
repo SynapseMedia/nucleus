57f5e99c33351527b54b5b7818a10843f6fac7fc
geolffrey <gmjun2000@gmail.com>
2021-02-27 10:47:08 -0600

    Fix hdb migration

--- src/core/orbit/migrate.js
+++ src/core/orbit/migrate.js
@@ -1,70 +1,80 @@
 const argv = require('minimist')(process.argv.slice(2));
 
 const MAX_CHUNKS = 1000
-const DB_MOVIES = 'wt.movies.db'
-const SOURCE_DB = argv.source || 'ipfs';
+const ORBIT_DB_NAME = 'wt.movies.db'
+const MIGRATE_FROM_DB = argv.source || 'ipfs';
+
 const IPFS_NODE = argv.node || 'ipfs'
-const KEY = argv.key || 'watchit' // Local key used to IPNS publish
-const MONGO_DB = argv.hdb || 'mongodb' // Temporary helper db
+const MONGO_HOST = argv.hdb || 'mongodb' // Temporary helper db
+
 const PDM = argv.p // Activate PDM filter
 const RECREATE = argv.r || true // Recreate database
+const KEY = argv.key || 'watchit' // Local key used to IPNS publish
 const REGEN = argv.g || false
 
-const ligLogger = require('@liquicode/lib-logger');
-const logs = ligLogger.NewConsoleLogger()
+
 const IpfsApi = require('ipfs-http-client');
 const OrbitDB = require('orbit-db');
 const {consume} = require('streaming-iterables')
 const MongoClient = require('mongodb').MongoClient;
+const logs = require('pino')({prettyPrint: {colorize: true}})
 const ipfs = IpfsApi({host: IPFS_NODE, port: '5001', protocol: 'http'});
 const msgpack = require("msgpack-lite");
 const {v4: uuidv4} = require('uuid');
 
 
 (async () => {
+
+    logs.info(`Connecting ipfs node`);
+    const chunkGen = (_movies, l) => {
+        return new Array(Math.ceil(_movies.length / l)).fill(0)
+            .map((_, n) => _movies.slice(n * l, n * l + l));
+    }
+
+    // Create OrbitDB instance
+    const DB_NAME = MIGRATE_FROM_DB;
+    const DB_OPTIONS = {overwrite: RECREATE, localOnly: false, replicate: true}
+    const orbitdb = await OrbitDB.createInstance(ipfs, {
+        directory: REGEN ? `./orbit${uuidv4()}` : './orbit'
+    });
+
+    // DB
+    const db = await orbitdb.log(ORBIT_DB_NAME, DB_OPTIONS);
+    db.events.on('peer', (p) => logs.warn('Peer Db:', p));
+
+    // END DB
+    const definedType = PDM ? 'PDM' : 'All';
+    logs.info(`Starting ${definedType} db `);
+    const dbAddress = db.address.toString()
+    const dbAddressHash = dbAddress.split('/')[2]
+
+    //Add provider to allow nodes connect to it
+    logs.info(`Providing address for ${definedType}`);
+    await consume(ipfs.dht.provide(dbAddressHash))
+    logs.info(`Publishing address for ${definedType}`)
+    const ipns = await ipfs.name.publish(dbAddressHash, {key: KEY})
+    logs.warn(`Publish done for for ${definedType}`, ipns.name)
+
+
+    // Start movies migration to orbit from mongo
+    let index = 0; // Keep cursor for movies id
+    const url = `mongodb://${MONGO_HOST}`;
+    const client = new MongoClient(url, {
+        useUnifiedTopology: true,
+        keepAlive: true
+    });
+
     try {
-        logs.info(`Connecting ipfs node`);
-        const chunkGen = (_movies, l) => {
-            return new Array(Math.ceil(_movies.length / l)).fill(0)
-                .map((_, n) => _movies.slice(n * l, n * l + l));
-        }
+        logs.warn('Connecting to helper db..');
+        await client.connect(async (err) => {
 
-        // Create OrbitDB instance
-        const DB_NAME = SOURCE_DB;
-        const DB_OPTIONS = {overwrite: RECREATE, localOnly: false, replicate: true}
-        const orbitdb = await OrbitDB.createInstance(ipfs, {
-            directory: REGEN ? `./orbit${uuidv4()}` : './orbit'
-        });
-
-        // DB
-        const db = await orbitdb.log(DB_MOVIES, DB_OPTIONS);
-        db.events.on('peer', (p) => logs.warn('Peer Db:', p));
-
-        // END DB
-        const definedType = PDM ? 'PDM' : 'W';
-        logs.info(`Starting ${definedType} db `);
-        const dbAddress = db.address.toString()
-        const dbAddressHash = dbAddress.split('/')[2]
-
-        //Add provider to allow nodes connect to it
-        logs.info(`Providing addressfor ${definedType}`);
-        await consume(ipfs.dht.provide(dbAddressHash))
-        logs.info(`Publishing address for ${definedType}`)
-        const ipns = await ipfs.name.publish(dbAddressHash, {key: KEY})
-        logs.warn(`Publish done for for ${definedType}`, ipns.name)
-
-
-        // Start movies migration to orbit from mongo
-        logs.trace('Saving data..');
-        let index = 0; // Keep cursor for movies id
-        const url = `mongodb://${MONGO_DB}`;
-        const client = new MongoClient(url, {useUnifiedTopology: true});
-        await client.connect(async () => {
+            console.log(DB_NAME)
             // Generate cursor for all movies
             const adminDb = client.db(DB_NAME)
             const cursor = adminDb.collection('movies').find(
                 {...PDM && {pdm: true}}
             ).limit(0).sort({year: 1})
+
             const size = await cursor.count();
             const data = chunkGen(await cursor.toArray(), MAX_CHUNKS);
             logs.info('Total movies:', size)
@@ -88,7 +98,6 @@ const {v4: uuidv4} = require('uuid');
                 });
 
                 //Add movie
-                console.log('Adding to network');
                 let cid = await ipfs.add(
                     msgpack.encode(ch),
                     {pin: true}
@@ -105,14 +114,16 @@ const {v4: uuidv4} = require('uuid');
                 // console.log('Created: ', cid.cid.toString());
             }
 
-            await client.close();
-            logs.info('Processed: ', `${index}/${size}`);
-            logs.warn('Address:', `${dbAddressHash}`)
-            logs.trace('Closed db..');
-        });
+        })
+
+        logs.info('Processed: ', `${index}/${size}`);
+        logs.warn('Address:', `${dbAddressHash}`)
 
     } catch (err) {
         logs.error(err);
+    } finally {
+        await client.close();
+        logs.warn('Closed db..');
     }
 
 })()
