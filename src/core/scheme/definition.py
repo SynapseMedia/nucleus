8342881c6947c14e4363fa62e9efc98670401252
geolffrey <gmjun2000@gmail.com>
2021-03-03 13:38:26 -0600

    Refactor definition.py

--- src/core/scheme/definition.py
+++ src/core/scheme/definition.py
@@ -25,40 +25,40 @@ DEFAULT_GENRES = [
 ]
 
 
-class ResourceScheme(Schema):
-    index = fields.Str()  # File index in CID directory
+class GenericScheme(Schema):
+    """
+    Generic abstract resource class definition
+    :type route: Define how to reach the resource eg: cid | uri
+    :type index: This is the index file name definition
+    """
+    route = fields.Str(required=True)  # Could be cid | uri
+    index = fields.Str()  # File index in directory
     abs = fields.Bool(default=False)
 
 
-class CIDScheme(ResourceScheme):
-    cid = fields.Str()  # CID hash
-
-
-class URIScheme(ResourceScheme):
-    url = fields.Url(relative=True)  # File link
-
-
-class ImageScheme(CIDScheme, URIScheme):
-    """
-    Image resource definition
-    """
-    pass
-
-
-class VideoScheme(CIDScheme, URIScheme):
+class VideoScheme(GenericScheme):
     """
     Video resource definition
+    :type quality: Optional attribute if .m3u8 match in `index` or `uri`
+    :type type: Mechanism to stream video eg: hls | torrent
     """
-    quality = fields.Str(required=True)  # Quality ex: 720p, 1080p..
+    quality = fields.Str(required=False)  # Quality ex: 720p, 1080p..
     type = fields.Str(validate=validate.OneOf(ALLOWED_FORMATS))
+
+
+class ImageCollectionScheme(Schema):
+    small_image = fields.Nested(GenericScheme)
+    medium_image = fields.Nested(GenericScheme)
+    large_image = fields.Nested(GenericScheme)
+
+
+class ResourceScheme(Schema):
+    images = fields.Nested(ImageCollectionScheme)
+    videos = fields.List(fields.Nested(VideoScheme))
 
 
 class MovieScheme(Schema):
     title = fields.Str(validate=validate.Length(min=1))
-    # Optional resource id to keep linked ex: origin?id=45
-    resource_id = fields.Int(missing=0)
-    # Where the data comes from?
-    resource_name = fields.Str(validate=validate.Length(min=2))
     # https://es.wikipedia.org/wiki/Internet_Movie_Database
     imdb_code = fields.Str(validate=validate.Regexp(r'^tt[0-9]{5,10}$'))
     rating = fields.Float(validate=validate.Range(min=0, max=DEFAULT_RATE_MAX))
@@ -73,8 +73,5 @@ class MovieScheme(Schema):
     language = fields.Str(validate=validate.Length(min=2, max=10))
     # https://en.wikipedia.org/wiki/Motion_Picture_Association_film_rating_system
     mpa_rating = fields.Str(default='PG')
-    small_image = fields.Nested(ImageScheme)
-    medium_image = fields.Nested(ImageScheme)
-    large_image = fields.Nested(ImageScheme)
-    resource = fields.List(fields.Nested(VideoScheme))
+    resource = fields.Nested(ResourceScheme)
     date_uploaded_unix = fields.Int(required=True)
