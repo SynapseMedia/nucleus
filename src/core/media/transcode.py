f99ce0d0bbb7a31bfbcae6e04084ebfa427d7ea8
phillmac <phillmackintosh@gmail.com>
2021-06-26 03:08:57 +0000

    Detect h264 and use vcodec copy mode

--- src/core/media/transcode.py
+++ src/core/media/transcode.py
@@ -22,12 +22,17 @@ def to_hls(input_file, output_dir):
 
     if file_format == DEFAULT_HLS_FORMAT:  # transcode hls from hls?
         logger.warning(f"Invalid source format {file_format}")
-
-    total_duration = float(ffmpeg.probe(input_file)['format']['duration'])
+    probe = ffmpeg.probe(input_file)
+    total_duration = float(probe['format']['duration'])
+    video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+    output_args = {}
+    if video_stream['codec_name'] == 'h264':
+        output_args['vcodec'] = 'copy'
+        logger.info('vcodec copy mode enabled')
     with helper.transcoder.show_progress(total_duration) as socket_filename:
         try:
             (ffmpeg.input(input_file)
-             .output(output_dir, format=DEFAULT_FORMAT, start_number=0, hls_time=DEFAULT_HLS_TIME, hls_list_size=0)
+             .output(output_dir, format=DEFAULT_FORMAT, start_number=0, hls_time=DEFAULT_HLS_TIME, hls_list_size=0, **output_args)
              .global_args('-progress', 'unix://{}'.format(socket_filename))
              .run(overwrite_output=True, capture_stdout=True, capture_stderr=True))
         except ffmpeg.Error as e:
