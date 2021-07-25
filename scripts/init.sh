0233d76137b941ac6f8f31ab74cf6a1ed55bdee7
geolffrey <gmjun2000@gmail.com>
2021-07-24 20:41:54 -0600

    feat: pin IPFS from prod dir

--- scripts/init.sh
+++ scripts/init.sh
@@ -10,6 +10,7 @@ ipfs config Peering.Peers "$peers" --json
 ipfs config --bool Swarm.EnableRelayHop false
 ipfs config --bool Swarm.EnableAutoRelay true
 ipfs config --bool Swarm.DisableBandwidthMetrics true
+ipfs config Swarm.AddrFilters "[]" --json
 ipfs config Swarm.ConnMgr.HighWater 160 --json
 ipfs config Swarm.ConnMgr.LowWater 80 --json
 ipfs config Datastore.GCPeriod "72h"
