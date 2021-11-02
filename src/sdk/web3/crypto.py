6f7f890e8879d93df41b1aba8078b9d131766337
gmena <gmjun2000@gmail.com>
2021-11-02 15:56:43 -0600

    feat: deterministic crypto base16 => hex => int

--- src/sdk/web3/crypto.py
+++ src/sdk/web3/crypto.py
@@ -1,19 +1,9 @@
-import base58
 from cid import make_cid
 from web3 import Web3
 
 
 def to_hex(_input):
     return Web3.toHex(_input)
-
-
-def base58_to_hex(b58):
-    """
-    Encode base58 to hex => uint256
-    :param b58: string to encode
-    :return: Hex string
-    """
-    return to_hex(base58.b58decode(b58)[2:])
 
 
 def cid_to_uint256(cid):
