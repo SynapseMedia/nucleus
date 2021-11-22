233ecdcca7d765044b3fd5d4ce836f0d20df16fd
gmena <gmjun2000@gmail.com>
2021-11-22 10:59:51 -0600

    chore: fix websocket

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,8 +7,9 @@ ipfs config Addresses.API '/ip4/0.0.0.0/tcp/5001'
 ipfs config Addresses.Gateway '/ip4/0.0.0.0/tcp/8080'
 # shellcheck disable=SC2016
 ipfs config Peering.Peers "$peers" --json
-ipfs config --bool Swarm.EnableRelayHop false
+ipfs config --bool Swarm.EnableRelayHop true
 ipfs config --bool Swarm.EnableAutoRelay true
+ipfs config --bool Swarm.EnableAutoNATService true
 ipfs config --bool Swarm.DisableBandwidthMetrics true
 ipfs config Swarm.AddrFilters "[]" --json
 ipfs config Pubsub.Router "gossipsub"
