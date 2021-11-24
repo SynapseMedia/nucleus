6e60c2e48a2f57aa6cca98cd8c3fcce49d7c9022
gmena <gmjun2000@gmail.com>
2021-11-24 15:56:20 -0600

    chore: fix websocket

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # shellcheck disable=SC2034
 peers=`cat /peering/peers.json`
-[ ! -e "/ipfsdata/config" ] && ipfs init
+[ ! -e "/ipfsdata/config" ] && ipfs init --profile server
 ipfs config Addresses.Swarm '["/ip4/0.0.0.0/tcp/4001", "/ip4/0.0.0.0/tcp/4002/ws", "/ip6/::/tcp/4001"]' --json
 ipfs config Addresses.API '/ip4/0.0.0.0/tcp/5001'
 ipfs config Addresses.Gateway '/ip4/0.0.0.0/tcp/8080'
 # shellcheck disable=SC2016
 ipfs config Peering.Peers "$peers" --json
-ipfs config --bool Swarm.EnableRelayHop true
+ipfs config --bool Swarm.EnableRelayHop false
 ipfs config --bool Swarm.EnableAutoRelay true
 ipfs config --bool Swarm.EnableAutoNATService true
 ipfs config --bool Swarm.DisableBandwidthMetrics true
