cb9677a208dac676a1687c76c3cb9e4d895ef1b2
gmena <gmjun2000@gmail.com>
2021-12-21 17:47:33 -0600

    chore: allow set init with profile node

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # shellcheck disable=SC2034
+profile=$1
+# shellcheck disable=SC2006
 peers=`cat /peering/peers.json`
-[ ! -e "/ipfsdata/config" ] && ipfs init --profile server
+[ ! -e "/ipfsdata/config" ] && ipfs init --profile "$profile"
 ipfs config Addresses.Swarm '["/ip4/0.0.0.0/tcp/4001", "/ip4/0.0.0.0/tcp/4002/ws", "/ip6/::/tcp/4001"]' --json
 ipfs config Addresses.API '/ip4/0.0.0.0/tcp/5001'
 ipfs config Addresses.Gateway '/ip4/0.0.0.0/tcp/8080'
