b6dbb8eb2eda438ef5ba9368b127894558deffa7
gmena <gmjun2000@gmail.com>
2021-12-23 10:00:54 -0600

    fix: handle ipfs exception

--- src/sdk/media/storage/ipfs.py
+++ src/sdk/media/storage/ipfs.py
@@ -3,7 +3,7 @@ import typing
 import docker
 import json
 from src.sdk.constants import IPFS_CONTAINER
-from src.sdk.exception import IpfsFailedExecution
+from src.sdk.exception import IPFSFailedExecution
 
 ipfs = "ipfs"
 
@@ -33,7 +33,7 @@ def exec_command(cmd, *args) -> typing.Union[dict, str]:
         0     Successful execution.
         1     Failed executions.
         """
-        raise IpfsFailedExecution(output.decode('utf-8'))
+        raise IPFSFailedExecution(output.decode('utf-8'))
 
     # If not result just keep object output standard
     if not output:
