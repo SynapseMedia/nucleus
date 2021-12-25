cadbaba368c2d7d56596a367155fd99ab31b48d5
gmena <gmjun2000@gmail.com>
2021-12-25 10:27:59 -0600

    test: scheme util

--- src/sdk/tests/scheme/test_util.py
+++ src/sdk/tests/scheme/test_util.py
@@ -1,63 +1,66 @@
+import pytest
 from src.sdk.scheme import util
+from marshmallow.exceptions import ValidationError
+
+cid = "bafyjvzacdiboycugrim5jwmqc2si7f5p2c4d27panotak6t4coma"
+hls_dag = {
+    "Links": [
+        {"Name": "hls"},
+    ]
+}
+
+expected_hls_output = {
+    "route": cid,
+    "index": {
+        "hls": "/movie/hls/index.m3u8",
+    },
+}
+
+image_dag = {
+    "Links": [
+        {"Name": "small.jpg"},
+        {"Name": "medium.jpg"},
+        {"Name": "large.jpg"},
+    ]
+}
+
+expected_image_output = {
+    "route": cid,
+    "index": {
+        "small": "/image/small.jpg",
+        "medium": "/image/medium.jpg",
+        "large": "/image/large.jpg",
+    },
+}
 
 
 # Unit tests
 def test_fit_image_resource_from_dag(mocker):
     """Should return valid image scheme fit for MediaScheme"""
-    cid = "QMmmmTEST"
-    image_dag = {
-        "Links": [
-            {"Name": "small.jpg"},
-            {"Name": "medium.jpg"},
-            {"Name": "large.jpg"},
-        ]
-    }
-
-    expected_output = {
-        "route": cid,
-        "index": {
-            "small": "/image/small.jpg",
-            "medium": "/image/medium.jpg",
-            "large": "/image/large.jpg",
-        },
-    }
 
     mocker.patch("src.sdk.scheme.util.dag_get", return_value=image_dag)
-    assert util.fit_image_resource_from_dag(cid) == expected_output
+    assert util.fit_image_resource_from_dag(cid) == expected_image_output
 
 
 # Unit tests
 def test_fit_video_hls_resource_from_dag(mocker):
     """Should return valid video scheme fit for MediaScheme hls"""
-    cid = "QMmmmTEST"
-    hls_dag = {
-        "Links": [
-            {"Name": "hls"},
-        ]
-    }
-
-    expected_output = {
-        "route": cid,
-        "index": {
-            "hls": "/movie/hls/index.m3u8",
-        },
-    }
 
     mocker.patch("src.sdk.scheme.util.dag_get", return_value=hls_dag)
-    assert util.fit_video_resource_from_dag(cid) == expected_output
+    assert util.fit_video_resource_from_dag(cid) == expected_hls_output
 
 
 # Unit tests
 def test_fit_video_dash_resource_from_dag(mocker):
-    """Should return valid video scheme fit for MediaScheme hls"""
-    cid = "QMmmmTEST"
+    """Should return valid video scheme fit for MediaScheme dash"""
+
     dash_dag = {
         "Links": [
             {"Name": "dash"},
         ]
     }
 
-    expected_output = {
+    expected_dash_output = {
         "route": cid,
         "index": {
             "dash": "/movie/dash/index.mpd",
@@ -65,4 +68,37 @@ def test_fit_video_dash_resource_from_dag(mocker):
     }
 
     mocker.patch("src.sdk.scheme.util.dag_get", return_value=dash_dag)
-    assert util.fit_video_resource_from_dag(cid) == expected_output
+    assert util.fit_video_resource_from_dag(cid) == expected_dash_output
+
+
+# Unit tests
+def test_fit_resources_from_dag(mocker):
+    """Should return valid scheme fit for MultiMediaScheme"""
+
+    mocker.patch(
+        "src.sdk.scheme.util.fit_video_resource_from_dag",
+        return_value=expected_hls_output,
+    )
+    mocker.patch(
+        "src.sdk.scheme.util.fit_image_resource_from_dag",
+        return_value=expected_image_output,
+    )
+
+    assert util.fit_resources_from_dag(cid)
+
+
+# Unit tests
+def test_fail_fit_resources_from_dag(mocker):
+    """Should fail with invalid scheme fit for MultiMediaScheme"""
+
+    mocker.patch(
+        "src.sdk.scheme.util.fit_video_resource_from_dag",
+        return_value=expected_hls_output.pop("route"),
+    )
+    mocker.patch(
+        "src.sdk.scheme.util.fit_image_resource_from_dag",
+        return_value=expected_image_output,
+    )
+
+    with pytest.raises(ValidationError):
+        assert util.fit_resources_from_dag(cid)
