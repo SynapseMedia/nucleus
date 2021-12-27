9f00108ae67b97bdadef4688810b3f2efa9462f2
gmena <gmjun2000@gmail.com>
2021-12-27 17:14:28 -0600

    chore: rename container input

--- src/sdk/expose/pin.js
+++ src/sdk/expose/pin.js
@@ -34,7 +34,9 @@ const logs = require('./logger')
         logs.info(`Resolved orbit address: ${_address}`)
         const orbitdb = await OrbitDB.createInstance(ipfs);
         logs.info(`Opening database from ${_address}`)
-        const db = await orbitdb.log(`/orbitdb/${_address}/wt.movies.db`, {sync: true})
+        const db = await orbitdb.log(`/orbitdb/${_address}/wt.movies.db`, {
+            sync: true, replicate: true, overwrite: true
+        })
 
         logs.info('Listening for updates to the database...')
         db.events.on('ready', () => logs.info("Db ready"))
