d524643a4d145dad5bbe1122290d292d340cede0
gmena <gmjun2000@gmail.com>
2021-12-29 10:11:07 -0600

    feat: unpin entries from edge

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@
 
 # shellcheck disable=SC2006
 peers=`cat /peering/peers.json`
-[ ! -e "/ipfsdata/config" ] && ipfs init --empty-repo --profile server
+[ ! -e "/ipfsdata/config" ] && ipfs init --empty-repo
 ipfs config Addresses.Swarm '["/ip4/0.0.0.0/tcp/4001", "/ip4/0.0.0.0/tcp/4002/ws", "/ip6/::/tcp/4001"]' --json
 ipfs config Addresses.API '/ip4/0.0.0.0/tcp/5001'
 ipfs config Addresses.Gateway '/ip4/0.0.0.0/tcp/8080'
@@ -24,7 +24,7 @@ ipfs config Swarm.AddrFilters '[
 
 
 ipfs config Datastore.GCPeriod "48h"
-ipfs config Datastore.StorageMax "2000GB"
+ipfs config Datastore.StorageMax "3000GB"
 ipfs config Datastore.StorageGCWatermark 99 --json
 ipfs config Pubsub.Router "gossipsub"
 ipfs config --json Swarm.DisableBandwidthMetrics false
