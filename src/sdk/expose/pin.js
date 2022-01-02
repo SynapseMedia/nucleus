4386d39c594cf9d739024abc0149b38bc198d2f7
gmena <gmjun2000@gmail.com>
2022-01-02 11:30:37 -0600

    feat: profile endpoints

--- src/sdk/expose/pin.js
+++ src/sdk/expose/pin.js
@@ -13,55 +13,62 @@ const logs = require('./logger')
 
 // List of default keys
 // ; = ensures the preceding statement was closed
-;(async () => {
-    async function runMapper() {
-        // Get monitor CID from IPNS
-        logs.info(`Resolving CID mapper ${MONITOR_CID}`)
-        const resolvedCid = await last(ipfs.name.resolve(MONITOR_CID))
-        const cid = resolvedCid.split('/').pop()
-        const addressIPNSList = await last(ipfs.cat(cid))
-        const addressListString = addressIPNSList.toString()
 
-        for (const address of addressListString.split('\n')) {
-            if (!address) continue
+const pin = async (cid) => {
+    logs.info(`Fetching block ${cid}`)
+    await ipfs.pin.add(cid)
+    logs.info(`Pinning hash ${cid}`)
+}
 
-            logs.info(`Resolving address from IPNS: ${address}`)
-            const cid = await last(ipfs.name.resolve(address))
-            const cleanedCID = cid.split('/').pop()
-            const newCID = CID.parse(cleanedCID)
-            const _address = newCID.toString(base58btc)
+const iterateOverReplica = async (db) => {
+    db.iterator({limit: -1}).collect().map(async (e) => {
+        await pin(e.payload.value)
+    })
+}
 
-            logs.info(`Resolved orbit address: ${_address}`)
-            const orbitdb = await OrbitDB.createInstance(ipfs);
+async function runMapper() {
+    // Get monitor CID from IPNS
+    logs.info(`Resolving CID mapper ${MONITOR_CID}`)
+    const resolvedCid = await last(ipfs.name.resolve(MONITOR_CID))
+    const cid = resolvedCid.split('/').pop()
+    const addressIPNSList = await last(ipfs.cat(cid))
+    const addressListString = addressIPNSList.toString()
 
-            logs.info(`Opening database from ${_address}`)
-            const db = await orbitdb.log(`/orbitdb/${_address}/wt.movies.db`, {
-                sync: true,
-                overwrite: true,
-                localOnly: false
-            })
+    for (const address of addressListString.split('\n')) {
+        if (!address) continue
 
-            logs.info('Listening for updates to the database...')
-            db.events.on('ready', () => {
-                db.iterator({limit: -1}).collect().map(async (e) => {
-                    const cid = e.payload.value
-                    logs.info(`Fetching block ${cid}`)
-                    await ipfs.pin.add(cid)
-                    logs.info(`Pinning hash ${cid}`)
-                })
-            })
+        logs.info(`Resolving address from IPNS: ${address}`)
+        const cid = await last(ipfs.name.resolve(address))
+        const cleanedCID = cid.split('/').pop()
+        const newCID = CID.parse(cleanedCID)
+        const _address = newCID.toString(base58btc)
 
-            await db.load()
+        logs.info(`Resolved orbit address: ${_address}`)
+        const orbitdb = await OrbitDB.createInstance(ipfs);
 
-        }
+        logs.info(`Opening database from ${_address}`)
+        const db = await orbitdb.log(`/orbitdb/${_address}/wt.movies.db`, {
+            sync: true,
+            overwrite: true,
+            localOnly: false
+        })
+
+        logs.info('Listening for updates to the database...')
+        db.events.on('ready', () => iterateOverReplica(db))
+        db.events.on('replicate.progress', (address, hash, entry) => pin(db.get(entry)))
+        await db.load()
     }
+}
 
 
-    runMapper().then(() => {
-        logs.info('Running Pinning Service')
-        setInterval(() => {
-            // Force restart docker
-            runMapper()
-        }, MONITOR_INTERVAL * 60 * 1000)
-    });
+;(async function start() {
+    logs.info('Running Pinning Service')
+    await runMapper() // Initial mapper start
+    logs.info('Setting interval')
+    setInterval(async () => {
+        // Force restart docker
+        await ipfs.stop()
+        await start()
+    }, MONITOR_INTERVAL * 60 * 1000)
 })()
+
