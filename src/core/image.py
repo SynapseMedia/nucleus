b0cf372e463258e6793ae74ffd9fc727a00f4ea8
geolffreym <gmjun2000@gmail.com>
2022-07-02 13:31:22 -0600

    test: image module

--- src/core/image.py
+++ src/core/image.py
@@ -1,6 +1,8 @@
-from PIL import Image
+from PIL.Image import Image, open
+from typing import Iterator
 from contextlib import contextmanager
 from dataclasses import dataclass
+from .exceptions import InvalidImageSize
 from .types import Directory
 
 
@@ -12,11 +14,25 @@ class Size:
 
 
 @contextmanager
-def input(input_image: Directory):
+def input(input_image: Directory) -> Iterator[Image]:
     """
     Factory Image
     :param input_image: Path to image
     :return: PIL Image object
     :rtype: Image
+    :raises InvalidImageSize
     """
-    yield Image.open(input_image)
+
+    with open(input_image) as image:
+        # input ratio size
+        w, h = image.size
+        # ratio height should be major than width
+        is_h_less_than_w = h < w
+        # input image is smaller than "master" 
+        is_input_less_than_master = any(x < y for x, y in zip(image.size, Size.Large))
+        
+        if is_h_less_than_w or is_input_less_than_master: 
+            # Invalid image ratio height should be major than width
+            raise InvalidImageSize()
+        
+        yield image
