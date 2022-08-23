e2f0f72c5845e906646858c3c0348a2e858ea91e
geolffreym <gmjun2000@gmail.com>
2022-08-23 17:01:42 -0600

    refactor: add rollback called on error

--- src/core/tests/cache/test_decorators.py
+++ src/core/tests/cache/test_decorators.py
@@ -1,4 +1,5 @@
 import pytest
+from mock import patch
 from src.core.cache.database import Connection, is_open
 from src.core.cache.decorator import connected, atomic
 
@@ -27,12 +28,13 @@ def test_atomic():
 
 def test_atomic_rollback():
     """Should fail running statements and rollback"""
+    with patch("src.core.cache.database.sqlite3") as mock:
+        _roll = mock.connect().rollback()  # type: ignore
+        with pytest.raises(Exception) :
+            @atomic
+            def to_decorate_with_atomic(conn: Connection):
+                # Should pass the current connection to db
+                raise Exception("failing")
 
-    with pytest.raises(Exception):
-        @atomic
-        # TODO mock rollback called times
-        def to_decorate_with_atomic(conn: Connection):
-            # Should pass the current connection to db
-            raise Exception("failing")
-
-        to_decorate_with_atomic()
+            to_decorate_with_atomic()
+            _roll.assert_called()  # type: ignore
