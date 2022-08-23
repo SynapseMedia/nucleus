99816f626d0d58b7ea3bbbf74ba6449db068f062
geolffreym <gmjun2000@gmail.com>
2022-08-23 17:10:52 -0600

    refactor: add rollback called on error

--- src/core/tests/cache/test_decorators.py
+++ src/core/tests/cache/test_decorators.py
@@ -17,24 +17,31 @@ def test_connected():
 
 def test_atomic():
     """Should run statements in transactional atomic mode"""
+    with patch("src.core.cache.database.sqlite3") as mock:
+        mock.connect().execute().fetchone.return_value = (1,) # type: ignore
+        _commit = mock.connect().commit  # type: ignore 
 
-    @atomic
-    def to_decorate_with_atomic(conn: Connection):
-        # Should pass the current connection to db
-        assert conn.execute("SELECT 1").fetchone() == (1,)
+        @atomic
+        def to_decorate_with_atomic(conn: Connection):
+            # Should pass the current connection to db
+            assert conn.execute("SELECT 1").fetchone() == (1,)
 
-    to_decorate_with_atomic()
+        to_decorate_with_atomic()
+        _commit.assert_called()  # type: ignore
 
 
 def test_atomic_rollback():
     """Should fail running statements and rollback"""
     with patch("src.core.cache.database.sqlite3") as mock:
-        _roll = mock.connect().rollback()  # type: ignore
-        with pytest.raises(Exception) :
+        _roll = mock.connect().rollback  # type: ignore
+
+        with pytest.raises(Exception):
+
             @atomic
-            def to_decorate_with_atomic(conn: Connection):
+            def to_decorate_with_atomic(_):
                 # Should pass the current connection to db
-                raise Exception("failing")
+                raise Exception("fail")
+                ...
 
             to_decorate_with_atomic()
             _roll.assert_called()  # type: ignore
