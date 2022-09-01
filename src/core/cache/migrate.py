660652acb2c06eff6372f48ad89938d147cd90cf
geolffreym <gmjun2000@gmail.com>
2022-09-01 09:36:29 -0600

    refactor: rename type to domain name

--- src/core/cache/migrate.py
+++ src/core/cache/migrate.py
@@ -6,12 +6,25 @@ from .database import Connection, TABLES_SCRIPT, INDEX_SCRIPT
 
 @connected
 def tables(conn: Connection):
+    """Attempt to migrate tables from script file
+    
+    :rtype: Connection
+    :return: Connection object
+    :raises sqlite3.OperationalError: if migration fails
+    
+    """
     with files.read(TABLES_SCRIPT) as schema:
         conn.executescript(schema)
     return conn
 
 @connected
 def indexes(conn: Connection):
+    """Attempt to migrate indexes from script file
+    
+    :rtype: Connection
+    :return: Connection object
+    :raises sqlite3.OperationalError: if migration fails
+    """
     with files.read(INDEX_SCRIPT) as index:
         conn.executescript(index)
     return conn
