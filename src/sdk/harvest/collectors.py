c65b10b4f7dbcd0fe7bde0fbb749a9d3617468ea
geolffreym <gmjun2000@gmail.com>
2022-11-30 08:58:15 -0600

    feat: merge collectors

--- src/sdk/harvest/collectors.py
+++ src/sdk/harvest/collectors.py
@@ -1,23 +1,36 @@
 # import inspect
 import pkgutil
 import inspect
-from src.core.types import Generator, Any
-from .constants import COLLECTORS_PATH
+import itertools
 
-def submodules(path: str = COLLECTORS_PATH) -> Generator[Any, Any, Any]:
+from src.core.types import Iterator, Any, Dict
+from .constants import COLLECTORS_PATH
+from .types import Collector
+
+
+def merge(collectors: Iterator[Collector]) -> Iterator[Dict[Any, Any]]:
+    """Iterate over collectors and merge results
+
+    :param collectors: Obtained data from collectors iterators
+    :return: Merged collectors results
+    :rtype: List[Dict[Any, Any]]
+    """
+
+    return itertools.chain.from_iterable(collectors)
+
+
+def load(path: str = COLLECTORS_PATH) -> Iterator[Any]:
     """Import submodules from a given path and return module object
 
     :param path: The path to search for submodules.
     :returns: Generator of matched modules.
-    :rtype: Generator[Any, Any, Any]
+    :rtype: Iterator[Any]
     """
-    
+
     for module_finder, name, _ in pkgutil.iter_modules([path]):
         module = module_finder.find_module(name).load_module(name)  # type: ignore
 
         # Get the module collector class
         for _, obj in inspect.getmembers(module):
             if inspect.isclass(obj):
-                yield obj
-
-
+                yield obj()
--- src/tests/sdk/harvest/test_collectors.py
+++ src/tests/sdk/harvest/test_collectors.py
@@ -1,23 +1,32 @@
 import src.sdk.harvest.collectors as collectors
-from src.core.types import Any
+
+expected = {
+    "title": "A Fork in the Road",
+    "synopsis": "Baby loves have fun",
+    "imdb_code": "wtt00000000",
+    "genres": ["Action", "Comedy", "Crime"],
+    "creator_key": "0xee99ceff640d37edd9cac8c7cff4ed4cd609f435",
+    "speech_language": "en",
+    "release_year": 2010,
+    "runtime": 105.0,
+    "mpa_rating": "PG",
+    "rating": 6.0,
+}
 
 
-def test_submodules_collector(mocker: Any):
+def test_submodules_collector():
     """Should copy for local file and not attempt download"""
-    dummy_class = list(collectors.submodules())[0]
-    data = list(dummy_class())
-    
-    expected = {
-        "title": "A Fork in the Road",
-        "synopsis": "Baby loves have fun",
-        "imdb_code": "wtt00000000",
-        "genres": ["Action", "Comedy", "Crime"],
-        "creator_key": "0xee99ceff640d37edd9cac8c7cff4ed4cd609f435",
-        "speech_language": "en",
-        "release_year": 2010,
-        "runtime": 105.0,
-        "mpa_rating": "PG",
-        "rating": 6.0,
-    }
-
+    dummy = list(collectors.load())[0]
+    data = list(dummy)
     assert data == [expected]
+
+
+def test_collect_collector():
+    loaded_collectors = collectors.load()
+    data_merged = collectors.merge(loaded_collectors)
+    
+    # we expect only one dummy class result even if we pass two collector because
+    # the idea is that merge return
+    expected2 = expected.copy()
+    expected2.update({"title": "A in the Road"})
+    assert list(data_merged) == [expected, expected2]
