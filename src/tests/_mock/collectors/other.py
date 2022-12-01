f5bfee6559e457831d4a1e5c28c2650a9c1ecb0d
geolffreym <gmjun2000@gmail.com>
2022-12-01 09:59:25 -0600

    test: merge collectors

---
+++ src/tests/_mock/collectors/other.py
@@ -0,0 +1,23 @@
+# Convention for importing types
+class Other:
+    def __str__(self):
+        return "dummy"
+
+    def __iter__(self):
+        """Here could be implemented any logic to collect metadata."""
+        
+        dummy_data = [{
+            "title": "A in the Road",
+            "synopsis": "Baby loves have fun",
+            "imdb_code": "wtt00000000",
+            "genres": ["Action", "Comedy", "Crime"],
+            "creator_key": "0xee99ceff640d37edd9cac8c7cff4ed4cd609f435",
+            "speech_language": "en",
+            "release_year": 2010,
+            "runtime": 105.0,
+            "mpa_rating": "PG",
+            "rating": 6.0,
+        }]
+        
+        for raw in dummy_data:
+            yield raw
