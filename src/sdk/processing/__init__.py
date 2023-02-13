6f57c9905678af0d2947e82de92f1da8d29ecc83
geolffreym <gmjun2000@gmail.com>
2023-02-13 15:05:52 -0600

    refactor: move processing to sdk

--- src/sdk/processing/__init__.py
+++
@@ -1,5 +0,0 @@
-from .transcode import ffmpeg
-from .transcode.codec import to_dash, to_hls
-from .transcode.types import Input
-
-__all__ = ("ffmpeg", "Input", "to_dash", "to_hls")
---
+++ src/sdk/processing/transcode/__init__.py
@@ -0,0 +1,4 @@
+from .ffmpeg import input
+from .codec import to_dash, to_hls
+
+__all__ = ("input", "to_dash", "to_hls")
