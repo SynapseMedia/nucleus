d4d04f4eee123602f92756eac12a3de963d6bb51
geolffreym <gmjun2000@gmail.com>
2023-02-14 14:34:57 -0600

    feat: transactional batch save

--- src/sdk/harvest/collectors.py
+++ src/sdk/harvest/collectors.py
@@ -1,24 +1,12 @@
 import pkgutil
 import inspect
 import itertools
-import builtins
 
 
 from collections import defaultdict
-from src.core.types import Iterator, Any, Mapping, Union
+from src.core.types import Iterator, Any, Mapping
 from .constants import COLLECTORS_PATH
 from .types import Collector, Model
-
-
-def batch_save(e: Iterator[Model]) -> Iterator[Union[int, None]]:
-    """Exec batch insertion into database
-    WARN: This execution its handled by a loop
-
-    :param e: Entries to insert into database.
-    :return: Iterator with a boolean flag for each operation.
-    :rtype: Iterator[bool]
-    """
-    return builtins.map(lambda x: x.save(), e)
 
 
 def map(collectors: Iterator[Collector]) -> Mapping[str, Iterator[Model]]:
@@ -59,8 +47,7 @@ def load(path: str = COLLECTORS_PATH) -> Iterator[Collector]:
     """
 
     for module_finder, name, _ in pkgutil.iter_modules([path]):
-        module = module_finder.find_module(
-            name).load_module(name)  # type: ignore
+        module = module_finder.find_module(name).load_module(name)  # type: ignore
 
         # Get the module collector class
         for _, obj in inspect.getmembers(module):
