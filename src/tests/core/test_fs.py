99372efdf833edd66a8ae764d4c53bece6105d1f
geolffreym <gmjun2000@gmail.com>
2023-02-20 09:35:06 -0600

    refactor: add pickle as handler

--- src/tests/core/test_fs.py
+++ src/tests/core/test_fs.py
@@ -1,9 +1,6 @@
 import os
 import re
 import pytest
-import src.core.fs as fs
-
-from pathlib import Path
 from src.core.types import Path
 
 custom_dir = "src/tests/core"
@@ -15,7 +12,7 @@ invalid = Path("NOT_EXIST")
 
 def test_valid_read_file():
     """Should return a valid file content with valid directory"""
-    with fs.read(license) as content:
+    with license.read() as content:
         pattern = "GNU AFFERO GENERAL PUBLIC LICENSE"
         assert content is not None
         assert re.search(pattern, content)
@@ -24,28 +21,28 @@ def test_valid_read_file():
 def test_invalid_read_file():
     """Should raise FileNotFoundError exception with invalid directory"""
     with pytest.raises(FileNotFoundError):
-        with fs.read(invalid):
+        with invalid.read():
             ...
 
 
 # Unit tests
 def test_exists_file():
     """Should return True for valid path"""
-    existing_file = fs.exists(license)
+    existing_file = license.exists()
     assert existing_file is True
 
 
 # Unit tests
 def test_exists_invalid_file():
     """Should return False for invalid path"""
-    existing_file = fs.exists(invalid)
+    existing_file = invalid.exists()
     assert existing_file is False
 
 
 def test_make_destination_dir():
     """Should create directory"""
     new_dir = Path("assets/mock_test_dir/")
-    new_created_dir = fs.make(new_dir)
+    new_created_dir = new_dir.make()
     expected_new_path = Path(new_created_dir)
     assert expected_new_path.exists()
     os.rmdir(new_created_dir)
@@ -55,5 +52,5 @@ def test_extract_extension_for_file():
     """Should extract extension from file path"""
     expected = "png"
     provided = Path("watchit.png")
-    assert fs.extension(provided) == expected
-    assert fs.extension(image) == expected
+    assert provided.extension() == expected
+    assert image.extension() == expected
