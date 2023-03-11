20f84e41561de0182adf15e8bbb99d1117de3da8
geolffreym <gmjun2000@gmail.com>
2023-03-10 19:58:49 -0600

    feat: processing nodes ffmpeg

---
+++ src/sdk/processing/transform/pillow.py
@@ -0,0 +1,25 @@
+import PIL.Image as PIL
+
+from src.core.types import Path, Any, Adaptable
+
+
+class Image(Adaptable):
+
+    _path: Path
+    _interface: PIL.Image
+
+    def __init__(self, path: Path, **kwargs: Any):
+        self._path = path  # the context file path
+        self._interface = PIL.open(path, **kwargs)  # type: ignore
+
+    def __instancecheck__(self, instance: PIL.Image):
+        return isinstance(instance, self._interface.__class__)
+
+    def __chaining__(self, interface: PIL.Image):
+        self._interface = interface
+
+    def __getattr__(self, name: str):
+        """Delegate calls to underlying lib.
+        ref: https://pillow.readthedocs.io/en/stable/reference/Image.html
+        """
+        return getattr(self._interface, name)
