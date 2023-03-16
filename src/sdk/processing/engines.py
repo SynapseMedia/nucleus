6d6cc58c6b37d4d55e1fd8e7bcee5b3a715c0e9e
Geolffrey Mena <gmjun2000@gmail.com>
2023-03-16 14:07:45 -0600

    Update engines.py

--- src/sdk/processing/engines.py
+++ src/sdk/processing/engines.py
@@ -59,8 +59,7 @@ class Image(Engine[Pillow]):
             func = getattr(self._library, method)
             # pillow image chaining
             # all methods return a new instance of the Image class, holding the resulting image
-            # ref:
-            # https://pillow.readthedocs.io/en/stable/reference/Image.html#PIL.Image.Image
+            # ref: https://pillow.readthedocs.io/en/stable/reference/Image.html#PIL.Image.Image
             self._library = func(**dict(params))
 
     def save(self, path: Path) -> Media[Path]:
