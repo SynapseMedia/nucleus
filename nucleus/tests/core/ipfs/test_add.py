cdb30e280d2c6524859530909242a0fa10cbc4bb
geolffreym <gmjun2000@gmail.com>
2023-03-30 12:14:01 -0600

    feat: store local using rpc

--- nucleus/tests/core/ipfs/test_add.py
+++ nucleus/tests/core/ipfs/test_add.py
@@ -6,12 +6,10 @@ from nucleus.core.ipfs import Add, File
 
 
 @responses.activate
-def test_add(
-        rpc_ipfs_api_add_request_output: JSON,
-        mock_local_video_path: Path):
+def test_add(rpc_ipfs_api_add_request: JSON, mock_local_video_path: Path):
     """Should return a valid ipfs api response for valid file"""
 
     api = ipfs.rpc()  # call to local ipfs
     command = Add(File(mock_local_video_path))
     output = api(command)  # call the command in api
-    assert output == rpc_ipfs_api_add_request_output
+    assert output == rpc_ipfs_api_add_request
