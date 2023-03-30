28a4eefd32160772f4daff5532bd0d43d56ab207
geolffreym <gmjun2000@gmail.com>
2023-03-30 12:00:09 -0600

    feat: store local using rpc

---
+++ nucleus/tests/sdk/storage/test_store.py
@@ -0,0 +1,26 @@
+import nucleus.sdk.storage as storage
+
+from nucleus.core.types import Path, JSON
+from nucleus.sdk.storage import Stored
+from nucleus.sdk.harvest import File, MediaType
+
+
+def test_storage_file(rpc_ipfs_api_add_request: JSON, mock_local_video_path: Path):
+    """Should dispatch the right request based on storable input"""
+
+    # retrieve the storage node, by default local ipfs local node
+    local_node = storage.client()
+
+    # store a new file in local node
+    storable = File(route=mock_local_video_path, type=MediaType.VIDEO)
+    stored = local_node(storable)  # expected Stored output
+
+    output_hash = rpc_ipfs_api_add_request.get("Hash")
+    output_size = rpc_ipfs_api_add_request.get("Size")
+    output_name = rpc_ipfs_api_add_request.get("Name")
+
+    assert stored.cid.valid()
+    assert stored.cid == output_hash
+    assert stored.name == output_name
+    assert stored.size == output_size
+    assert isinstance(stored, Stored)
