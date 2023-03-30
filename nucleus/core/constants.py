0a5fbb6612ae41a536eb8f6b962c32fa172d012a
geolffreym <gmjun2000@gmail.com>
2023-03-30 12:32:22 -0600

    feat: store local using rpc

--- nucleus/core/constants.py
+++ nucleus/core/constants.py
@@ -1,6 +1,4 @@
-import os
 import pathlib
 
 # Http settings
 ROOT_DIR = pathlib.Path().resolve()  # relative root path
-VALIDATE_SSL = os.getenv("VALIDATE_SSL", "False") == "True"
---
+++ nucleus/tests/_mock/collectors/file.py
@@ -0,0 +1,23 @@
+import json
+from nucleus.sdk.harvest import Collector
+
+# class MyMeta(Meta):
+#     """You can define your own data model here .
+#     Write here any custom metadata model to distribute"""
+
+#     title: str
+#     address: str
+#     ...
+
+
+class File(Collector):
+    """File implements Collector interface"""
+
+    def __iter__(self):
+        """Here could be implemented any logic to collect metadata."""
+
+        source_file = "nucleus/tests/_mock/files/dummy.json"
+        with open(source_file) as file:
+            # read movies from json file
+            for raw in json.load(file):
+                yield raw
