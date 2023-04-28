51b399e2a5c723f6f2e97e4cc753e8abdf1e296a
geolffreym <gmjun2000@gmail.com>
2023-04-28 15:51:22 -0600

    feat: marshall standard distribution

---
+++ nucleus/sdk/expose/metadata.py
@@ -0,0 +1,39 @@
+from nucleus.core.types import CID, Optional, Dynamic
+
+
+class Structural(Dynamic):
+    """Structural metadata implementation.
+    https://github.com/SynapseMedia/sep/blob/main/SEP/SEP-001.md
+    """
+
+    cid: CID
+    path: Optional[str] = None
+
+    def __str__(self):
+        return "s"
+
+
+class Descriptive(Dynamic):
+    """Descriptive metadata implementation.
+    https://github.com/SynapseMedia/sep/blob/main/SEP/SEP-001.md
+    """
+
+    name: str
+    desc: str
+
+    def __str__(self):
+        return "d"
+
+
+class Technical(Dynamic):
+    """Technical metadata implementation.
+    https://github.com/SynapseMedia/sep/blob/main/SEP/SEP-001.md
+    """
+
+    size: int
+
+    def __str__(self):
+        return "t"
+
+
+__all__ = ["Structural", "Descriptive", "Technical"]
