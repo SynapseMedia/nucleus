401b5acee0bad5af475b2395b624e02eab164673
geolffreym <gmjun2000@gmail.com>
2023-05-18 17:38:30 -0600

    chore: remove scripts

--- scripts/bp.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Build and push script
-cd .
-docker build -f $1 . -t $2:$3
-docker push $2:$3
-
-
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.1.0"
+VERSION = "0.1.0-alpha"
 DESCRIPTION = "The building block for decentralized media"
 GITHUB_LINK = "https://github.com/SynapseMedia/nucleus"
 
@@ -19,4 +19,21 @@ setup(
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     zip_safe=False,
+    install_require=[
+        "ffmpeg-python==0.2.0",
+        "pydantic==1.10.1",
+        "hexbytes==0.2.2",
+        "multiformats==0.2.1",
+        "dag-cbor==0.3.2",
+        "Pillow==9.2.0",
+        "requests==2.27.1"
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+    ],
 )
