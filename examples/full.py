281450809f4dd5037d0624a83a6d80b164e272df
geolffreym <gmjun2000@gmail.com>
2023-05-20 11:27:53 -0600

    chore: installing pip

--- examples/full.py
+++ examples/full.py
@@ -66,7 +66,7 @@ def main():
         # standard implementation
         # https://github.com/SynapseMedia/sep/blob/main/SEP/SEP-001.md
         sep001 = expose.standard(media_type)  # image/png
-        sep001.set_method(DagJose)  # set serialization method
+        sep001.set_serialization(DagJose)  # set serialization method
         sep001.add_metadata(Descriptive(**dict(nucleus)))
         sep001.add_metadata(Structural(cid=stored_file_object.hash))
         sep001.add_metadata(Technical(size=size, width=width, height=height))
