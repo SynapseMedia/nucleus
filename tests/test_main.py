be6172b4087d5d35f5ebb74117704f496c9fd8d1
geolffreym <gmjun2000@gmail.com>
2023-06-27 13:56:57 -0600

    fix: object mapping instrospection

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,18 +13,17 @@ from nucleus.sdk.expose import (
     Structural,
     Technical,
 )
-from nucleus.sdk.harvest import Video, Model
-from nucleus.sdk.processing import Engine, File, HLS, H264
-from nucleus.sdk.storage import Object, Store, Client
+from nucleus.sdk.harvest import Model, Video
+from nucleus.sdk.processing import H264, HLS, Engine, File
+from nucleus.sdk.storage import Object, Store
 
 
 @pytest.mark.skip(reason='no way of currently testing this. mock needed')
 def test_main():
-    LOCAL_ENDPOINT = "http://localhost:5001"
-    FAKE_KEY = "EST2bf02f6d-c48d-4868-90c9-7015ab6d0b74ARY"
+    LOCAL_ENDPOINT = 'http://localhost:5001'
 
     # 1. prepare our model schema to collect/validate/clean data
-    with logger.console.status("Harvesting"):
+    with logger.console.status('Harvesting'):
 
         class Nucleus(Model):
             name: str
@@ -33,23 +32,23 @@ def test_main():
 
         # set our data in the model
         nucleus: Model = Nucleus(
-            name="Nucleus the SDK",
-            description="Building block for multimedia decentralization",
-            contributors=["Jacob", "Geo", "Dennis", "Mark"],
+            name='Nucleus the SDK',
+            description='Building block for multimedia decentralization',
+            contributors=['Jacob', 'Geo', 'Dennis', 'Mark'],
         )
 
     # 2. init our processing engine based on our media model
-    with logger.console.status("Processing"):
+    with logger.console.status('Processing'):
         # "infer" engine based on input media type
-        video: Video = harvest.video(path=Path("sample.mp4"))
+        video: Video = harvest.video(path=Path('sample.mp4'))
         image_engine: Engine = processing.engine(video)
         # transcode to HLS/H264
         image_engine.configure(HLS(H264()))
         # finally save the processed image to our custom dir
-        output_file: File = image_engine.save(Path("sample.m3u8"))
+        output_file: File = image_engine.save(Path('sample.m3u8'))
 
     # 3. store our processed image in local IPFS node and pin it in estuary
-    with logger.console.status("Storage"):
+    with logger.console.status('Storage'):
         local_storage: Store = storage.ipfs(LOCAL_ENDPOINT)
         stored_file_object: Object = local_storage(output_file)
         # choose and connect an edge service to pin our resources. eg. estuary
@@ -57,7 +56,7 @@ def test_main():
         # estuary.pin(stored_file_object)
 
     # 4. expose our media through the standard
-    with logger.console.status("Expose"):
+    with logger.console.status('Expose'):
         # technical information about image
         length = float(output_file.meta.format.duration) * 1000
         size = int(output_file.meta.size)
