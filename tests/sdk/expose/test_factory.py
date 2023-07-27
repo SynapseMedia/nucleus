9e89b84a73bfc747f4325b932088715137152a43
geolffreym <gmjun2000@gmail.com>
2023-07-27 09:15:42 -0600

    refactor: add import key method to Keyring

--- tests/sdk/expose/test_factory.py
+++ tests/sdk/expose/test_factory.py
@@ -1,14 +1,14 @@
 import nucleus.sdk.expose as expose
 from nucleus.sdk.expose import SignKeyRing
 
+
 def test_ecdsa_factory():
     """Should return an expected and valid SigKeyRing"""
-    
-    ecdsa = expose.es256()
-    
-    assert isinstance(ecdsa, SignKeyRing)
-    assert ecdsa.alg == "ES256"
-    assert ecdsa.crv == "P-256"
-    assert ecdsa.use == "sig"
-    assert ecdsa.kty == "EC"
 
+    ecdsa = expose.es256()
+
+    assert isinstance(ecdsa, SignKeyRing)
+    assert ecdsa.alg == 'ES256'
+    assert ecdsa.crv == 'P-256'
+    assert ecdsa.use == 'sig'
+    assert ecdsa.kty == 'EC'
